8686ec5fae00db7feab17258ad8b030acff8d0d7
TimothyFell <timothy.fell846@gmail.com>
2019-01-03 14:34:15 -0700

    Add google service and tests, still needs vcr

--- Gemfile
+++ Gemfile
@@ -12,12 +12,20 @@ gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'faraday'
+gem 'fast_jsonapi'
+gem 'figaro'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
+  gem 'faker'
+  gem 'factory_bot_rails'
+  gem 'pry'
+  gem 'simplecov'
+  gem 'shoulda-matchers'
 end
 
 group :development do
---
+++ spec/services/google_service_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'GoogleService' do
+  before(:each) do
+    @service = GoogleService.new
+  end
+
+  it 'exists' do
+    expect(@service).to be_a(GoogleService)
+  end
+
+  describe '#get_lat_lon' do
+
+    it 'should return latitude and longitude for a specified location' do
+      coords = @service.get_lat_lon('denver, co')
+
+      expect(coords).to be_a(Hash)
+      expect(coords[:lat]).to eq(39.7392358)
+      expect(coords[:lng]).to eq(-104.990251)
+    end
+
+  end
+end
